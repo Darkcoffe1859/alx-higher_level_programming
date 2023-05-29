f8640f45ce9de4cbd023a1eaa3f0ab0f528084ce
Darkcoffe1859 <shency49@gmail.com>
2023-05-29 08:00:34 -0700

    exceptions

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_python_bytes - print basic information about Python bytes objects
+ * @p: python object to print basic information about
+ * Return: nothing
+ **/
+void print_python_bytes(PyObject *p)
+{
+}
+/**
+ * print_python_float - print the Python float
+ * @p: python float
+ * Return: nothing
+ **/
+void print_python_float(PyObject *p)
+{
+}
+/**
+ * print_python_list - print basic information about Python lists
+ * @p: python object
+ * Return: nothing
+ **/
+void print_python_list(PyObject *p)
+{
+}
