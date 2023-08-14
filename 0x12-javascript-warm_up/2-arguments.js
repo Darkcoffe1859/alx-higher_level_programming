6b67d60e89f1bb0e1716e84fca115bc55f2467ef
Darkcoffe1859 <shency49@gmail.com>
2023-08-14 14:18:52 +0100

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (no arguments are passed to the script) {
-console.log (no argument);
-} else if (one argument is passed to the script) {
-console.log (Argument found)
-}else {
-console.log (Arguments found)
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found')
