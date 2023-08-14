278441bf65ff30eab50a65c96a52599ebaf29b0f
Darkcoffe1859 <shency49@gmail.com>
2023-08-14 15:41:48 +0100

    concat

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(parseint(process.argv[2]) ? 'My number: ${parseint(process.argv[2])}' :'Not a number');
+console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number');
